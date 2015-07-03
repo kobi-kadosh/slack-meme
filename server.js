0ea97d4e25a51e1047375c96372138fda195f20b
Kobi Kadosh <kobi.kadosh@gmail.com>
2015-07-03 04:00:07 +0300

    fixes for heroku

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('port', Config.http.port);
 var lib = new IndexLib(Config, app);
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(process.env.PORT || app.get('port'), function() {
 	console.log('Express server listening on port ' + server.address().port)
 });
